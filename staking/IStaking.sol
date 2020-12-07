cdca4e861a413d4c76a69fd5f9e4e74f496168a7
Chef Chansey <73282620+chefchansey@users.noreply.github.com>
2020-12-07 16:06:24 +0700

    Create IStaking.sol

---
+++ staking/IStaking.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.5.17;
+
+/**
+ * @title Staking interface, as defined by EIP-900.
+ * @dev https://github.com/ethereum/EIPs/blob/master/EIPS/eip-900.md
+ */
+
+contract IStaking {
+    event Staked(address indexed user, uint256 amount, uint256 total, bytes data);
+    event Unstaked(address indexed user, uint256 amount, uint256 total, bytes data);
+
+    function stake(uint256 amount, bytes calldata data) external;
+    function stakeFor(address user, uint256 amount, bytes calldata data) external;
+    function unstake(uint256 amount, bytes calldata data) external;
+    function totalStakedFor(address addr) public view returns (uint256);
+    function totalStaked() public view returns (uint256);
+    function token() external view returns (address);
+
+    /**
+     * @return False. This application does not support staking history.
+     */
+    function supportsHistory() external pure returns (bool) {
+        return false;
+    }
+}
