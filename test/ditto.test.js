4f1d1cd48a70f897d745cc39a1ae077bd3d1231a
osboxes.org <chefchansey@gmail.com>
2020-12-14 05:52:37 -0500

    Fix Ditto ERC20 tests

--- test/ditto.test.js
+++ test/ditto.test.js
@@ -15,44 +15,19 @@ contract('Ditto', ([alice, bob, carol]) => {
         assert.equal(decimals.valueOf(), '9');
     });
 
-    it('should only allow owner to mint token', async () => {
-        await this.ditto.mint(alice, '100', { from: alice });
-        await this.ditto.mint(bob, '1000', { from: alice });
-        await expectRevert(
-            this.ditto.mint(carol, '1000', { from: bob }),
-            'Ownable: caller is not the owner',
-        );
+    it('should have correct initial supply', async () => {
         const totalSupply = await this.ditto.totalSupply();
-        const aliceBal = await this.ditto.balanceOf(alice);
-        const bobBal = await this.ditto.balanceOf(bob);
-        const carolBal = await this.ditto.balanceOf(carol);
-        assert.equal(totalSupply.valueOf(), '1100');
-        assert.equal(aliceBal.valueOf(), '100');
-        assert.equal(bobBal.valueOf(), '1000');
-        assert.equal(carolBal.valueOf(), '0');
+        assert.equal(totalSupply.valueOf(), '1750000000000000');
+        assert.equal(aliceBal.valueOf(), totalSupply.valueOf());
     });
 
     it('should supply token transfers properly', async () => {
-        await this.ditto.mint(alice, '100', { from: alice });
-        await this.ditto.mint(bob, '1000', { from: alice });
-        await this.ditto.transfer(carol, '10', { from: alice });
-        await this.ditto.transfer(carol, '100', { from: bob });
-        const totalSupply = await this.ditto.totalSupply();
-        const aliceBal = await this.ditto.balanceOf(alice);
-        const bobBal = await this.ditto.balanceOf(bob);
+        await this.ditto.transfer(carol, '100', { from: alice });
         const carolBal = await this.ditto.balanceOf(carol);
-        assert.equal(totalSupply.valueOf(), '1100');
-        assert.equal(aliceBal.valueOf(), '90');
-        assert.equal(bobBal.valueOf(), '900');
-        assert.equal(carolBal.valueOf(), '110');
+        assert.equal(carolBal.valueOf(), '100');
     });
 
     it('should fail if you try to do bad transfers', async () => {
-        await this.ditto.mint(alice, '100', { from: alice });
-        await expectRevert(
-            this.ditto.transfer(carol, '110', { from: alice }),
-            'ERC20: transfer amount exceeds balance',
-        );
         await expectRevert(
             this.ditto.transfer(carol, '1', { from: bob }),
             'ERC20: transfer amount exceeds balance',
